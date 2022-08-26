fd51c64ba5c9625ffaf481ec27a39b9c410c6397
Boris Barac <boris.raptor@gmail.com>
2022-08-26 20:19:30 +0200

    Test improvments

--- test/api_test.py
+++ test/api_test.py
@@ -12,21 +12,36 @@ from api.validate import validate_password
 logging.basicConfig(level=logging.DEBUG)
 my_logger = logging.getLogger()
 
+_my_user = {
+    'email': 'boris123@email.com',
+    'password': 'password1234'
+}
+
 @pytest.fixture
 def client():
     BASE_DIR = Path(__file__).resolve().parent.parent
     app.config["TESTING"] = True
     yield app.test_client()  # tests run here
 
-# eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ1c2VyX2lkIjoiNjMwOGZkNWE4NThkZGUzMWNkYWE3NjhhIn0.82uhDM_99VnRmGbykALqhWL0tOoNcMV8od0shuEMzLc
+def test_login(client):
+    response = client.post("/users/login", json = _my_user)
+    res_data = response.json['data']
+    
+    assert res_data != None
+    assert res_data['token'] != None
+    assert response.status_code == 200
 
 def test_get_user(client):
+    response = client.post("/users/login", json = _my_user)
+    token = response.json.get('data', {}).get('token', '')
     header = {
-        'Authorization': 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ1c2VyX2lkIjoiNjMwOGZkNWE4NThkZGUzMWNkYWE3NjhhIn0.82uhDM_99VnRmGbykALqhWL0tOoNcMV8od0shuEMzLc'
+        'Authorization': token
     }
+
     response = client.get("/users/", headers = header)
     res_data = response.json['data']
     res_error = response.json.get('error', None)
+    
     assert res_data != None
     assert res_error == None
     assert response.status_code == 200
@@ -35,21 +50,9 @@ def test_get_user_no_token(client):
     response = client.get("/users/")
     res_data = response.json['data']
     res_error = response.json['error']
+    
     assert res_data == None
     assert res_error != None
-
-def test_login(client):
-    data = {
-        'email': 'boris123@email.com',
-        'password': 'password1234'
-    }
-
-    response = client.post("/users/login", json = data)
-
-    res_data = response.json['data']
-    assert res_data != None
-    assert res_data['token'] != None
-    assert response.status_code == 200
 
 def test_register_user(client):
     data = {
@@ -59,9 +62,10 @@ def test_register_user(client):
     }
 
     response = client.post("/users/", json = data)
-
     message = response.json['message']
+
     assert response.status_code == 201
+    assert message != None
 
 def test_register_user_error(client):
     data = {
@@ -71,15 +75,17 @@ def test_register_user_error(client):
     }
 
     response = client.post("/users/", json = data)
-
     message = response.json['message']
+
     assert 'Password is invalid' in message
     assert response.status_code == 401
 
 def test_regex():
     res = validate_password("borSSSis123u2913")
+    
     assert res == True
 
 def test_index(client):
     response = client.get("/")
+    
     assert response.status_code == 200
